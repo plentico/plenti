897bf7ea8c52c2233597ff9a0307848c2b78c059
jimafisk <jimafisk@gmail.com>
2020-04-04 12:23:28 -0400

    Update license notices.

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 Jantcu jim.fisk@jantcu.com
+Copyright © 2020 Jim Fisk <jim.fisk@jantcu.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
