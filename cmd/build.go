f38f97f7b826ae0fb50a6eb680e5b118ab221967
jimafisk <jimafisk@gmail.com>
2020-04-16 16:02:56 -0400

    Update error messages.

--- cmd/build.go
+++ cmd/build.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -48,8 +47,7 @@ you need to deploy for your website.`,
 		buildPath := filepath.Join(".", buildDir)
 		err := os.MkdirAll(buildPath, os.ModePerm)
 		if err != nil {
-			fmt.Printf("Unable to create \"%v\" build directory\n", buildDir)
-			log.Fatal(err)
+			fmt.Printf("Unable to create \"%v\" build directory: %s\n", buildDir, err)
 		} else {
 			fmt.Printf("Creating \"%v\" build directory\n", buildDir)
 		}
@@ -66,8 +64,6 @@ you need to deploy for your website.`,
 		for _, layoutFile := range layoutFiles {
 			// Create destination path.
 			destFile := buildPath + strings.Replace(layoutFile, "layout", "/spa", 1)
-			// Ensure current path is not a file (i.e. it's a directory).
-			//if !strings.Contains(layoutFile, ".") {
 			// Make sure path is a directory
 			fileInfo, _ := os.Stat(layoutFile)
 			if fileInfo.IsDir() {
@@ -78,25 +74,25 @@ you need to deploy for your website.`,
 			if filepath.Ext(layoutFile) == ".js" {
 				from, err := os.Open(layoutFile)
 				if err != nil {
-					log.Fatal(err)
+					fmt.Printf("Could not open source .js file for copying: %s\n", err)
 				}
 				defer from.Close()
 
 				to, err := os.Create(destFile)
 				if err != nil {
-					log.Fatal(err)
+					fmt.Printf("Could not create destination .js file for copying: %s\n", err)
 				}
 				defer to.Close()
 
 				_, fileCopyErr := io.Copy(to, from)
 				if err != nil {
-					log.Fatal(fileCopyErr)
+					fmt.Printf("Could not copy .js from source to destination: %s\n", fileCopyErr)
 				}
 			}
 			if filepath.Ext(layoutFile) == ".svelte" {
 				fileContentByte, readFileErr := ioutil.ReadFile(layoutFile)
 				if readFileErr != nil {
-					log.Fatal(readFileErr)
+					fmt.Printf("Could not read contents of svelte source file: %s\n", readFileErr)
 				}
 				fileContentStr := string(fileContentByte)
 				//fmt.Printf("contents: %s\n", fileContentStr)
@@ -114,73 +110,11 @@ you need to deploy for your website.`,
 
 		}
 
-		//exec.Command("npx", "snowpack", "--include", "'public/spa/**/*'", "--dest", "'public/spa/web_modules'").Output()
+		_, snowpackErr := exec.Command("npx", "snowpack", "--include", "'public/spa/**/*'", "--dest", "'public/spa/web_modules'").Output()
+		if snowpackErr != nil {
+			fmt.Printf("Snowpack failed to build npm dependencies: %s\n", snowpackErr)
+		}
 
-		/*
-					// Get settings from config file.
-					siteConfig := readers.GetSiteConfig()
-
-					// Check flags and config for directory to build to
-					buildDir := setBuildDir(siteConfig)
-
-					newpath := filepath.Join(".", buildDir)
-					err := os.MkdirAll(newpath, os.ModePerm)
-					if err != nil {
-						fmt.Printf("Unable to create \"%v\" build directory\n", buildDir)
-						log.Fatal(err)
-					} else {
-						fmt.Printf("Creating \"%v\" build directory\n", buildDir)
-					}
-
-					// TODO: replace hardcoded scaffolding
-					var publicHTML = map[string][]byte{
-						"/index.html": []byte(`<!DOCTYPE html>
-			<html>
-			  <head>
-			    <meta charset="utf-8">
-			    <meta name="viewport" content="width=device-width">
-			    <title>Home | Plenti</title>
-			  </head>
-			  <body>
-			    <h1>Welcome to Plenti</h1>
-			    <p>Run <pre>npm install</pre> and <pre>npm run dev</pre> to get started.</p>
-			    <p><a href="/about">About us</a>.</p>
-			    <div id="app"></div>
-			    <script src="/dist/bundle.js"></script>
-			  </body>
-			</html>`),
-						"/about/index.html": []byte(`<!DOCTYPE html>
-			<html>
-			  <head>
-			    <meta charset="utf-8">
-			    <meta name="viewport" content="width=device-width">
-			    <title>About | Plenti</title>
-			  </head>
-			  <body>
-			    <h1>About page</h1>
-			    <p><a href="/">Go home</a>.</p>
-			    <div id="app"></div>
-			    <script src="/dist/bundle.js"></script>
-			  </body>
-			</html>`),
-					}
-					for file, content := range publicHTML {
-						subDirs := strings.Split(file, "/")
-						prevDir := newpath
-						for _, subDir := range subDirs {
-							// If a file extension exists, don't create directory
-							if strings.Contains(subDir, ".") {
-								break
-							}
-							os.MkdirAll(prevDir+subDir, os.ModePerm)
-							prevDir = prevDir + "/" + subDir
-						}
-						err := ioutil.WriteFile(newpath+file, content, 0755)
-						if err != nil {
-							fmt.Printf("Unable to write file: %v", err)
-						}
-					}
-		*/
 	},
 }
 
