5c219f23c6947184e1a5c26d1007c549c9a41e4f
jimafisk <jimafisk@gmail.com>
2020-05-14 11:24:19 -0400

    Simplify benchmark and add comments.

--- cmd/build/benchmark.go
+++ cmd/build/benchmark.go
@@ -2,31 +2,30 @@ package build
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-var doOnce sync.Once
-
-var benchmarkFlag bool = false
+var benchmarkFlag bool
 var messages []string
-var allElapsed []time.Duration
+var elapsed []time.Duration
 
 // Benchmark records time for individual build processes.
 func Benchmark(start time.Time, message string, BenchmarkFlags ...bool) {
 
-	elapsed := time.Since(start)
-	allElapsed = append(allElapsed, elapsed)
+	elapsed = append(elapsed, time.Since(start))
 	messages = append(messages, message)
 
 	// Get length of variadic args (used to mimic optional parameters).
 	numFlags := len(BenchmarkFlags)
+	// If the --benchmark flag is true.
 	if numFlags == 1 && BenchmarkFlags[0] {
+		// Show time spent on each stage of build process.
 		for i, m := range messages {
-			fmt.Printf("\n%s took %s\n", m, allElapsed[i])
+			fmt.Printf("\n%s took %s\n", m, elapsed[i])
 		}
 	} else if numFlags == 1 {
-		fmt.Printf("\n%s took %s\n", message, elapsed)
+		// If the --benchmark flag is false, just print overall build time.
+		fmt.Printf("\n%s took %s\n", message, elapsed[0])
 	}
 
 }
