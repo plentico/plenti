5c1feffae0c8261e4a8ff87489172481689c8b2e
jimafisk <jimafisk@gmail.com>
2020-05-23 12:31:41 -0400

    Implement Unmarshaler interface to filter out non string vals from JSON.

--- readers/type_fields.go
+++ readers/type_fields.go
@@ -10,14 +10,35 @@ type TypeFields struct {
 	Fields map[string]string `json:",string"`
 }
 
+// UnmarshalJSON method filters out non-string values from content source.
+func (t *TypeFields) UnmarshalJSON(data []byte) error {
+	var unknownValues map[string]interface{}
+	if err := json.Unmarshal(data, &unknownValues); err != nil {
+		return err
+	}
+	t.Fields = map[string]string{}
+	for field, unknownValue := range unknownValues {
+		_, isString := unknownValue.(string)
+		if isString {
+			t.Fields[field] = fmt.Sprintf("%v", unknownValue)
+			fmt.Printf("Its a string: %s", field)
+		}
+	}
+	fmt.Printf("I HAVE RUN!!!!")
+	return nil
+}
+
 // GetTypeFields reads the key/values for an individual content type JSON file.
 func GetTypeFields(typeFileContents []byte) TypeFields {
 
 	var typeFields TypeFields
-	err := json.Unmarshal(typeFileContents, &typeFields.Fields)
-	if err != nil {
-		fmt.Printf("Unable to read content type source file: %s\n", err)
-	}
+	typeFields.UnmarshalJSON(typeFileContents)
+	/*
+		err := json.Unmarshal(typeFileContents, &typeFields.Fields)
+		if err != nil {
+			fmt.Printf("Unable to read content type source file: %s\n", err)
+		}
+	*/
 
 	return typeFields
 }
