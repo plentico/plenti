b18871b78adda0f404ea5314c5332ed7e7283d64
jimafisk <jimafisk@gmail.com>
2020-06-30 18:37:03 -0400

    Login to dockerhub in github action.

--- scripts/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
-    echo "Login to hub.docker.com"
-    echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-fi
