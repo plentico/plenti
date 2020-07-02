83d8ebb6991ef3be35ee4f1cb978b4198e748691
jimafisk <jimafisk@gmail.com>
2020-07-02 15:55:16 -0400

    Run script with v8.

--- cmd/build/exec_node.go
+++ cmd/build/exec_node.go
@@ -1,9 +1,11 @@
 package build
 
 import (
-	"os"
-	"os/exec"
+	"fmt"
+	"io/ioutil"
 	"time"
+
+	"rogchap.com/v8go"
 )
 
 // ExecNode runs a build script written in NodeJS that compiles svelte.
@@ -11,8 +13,21 @@ func ExecNode(clientBuildStr string, staticBuildStr string, allNodesStr string) 
 
 	defer Benchmark(time.Now(), "Compiling components and creating static HTML")
 
-	svelteBuild := exec.Command("node", "ejected/build.js", clientBuildStr, staticBuildStr, allNodesStr)
-	svelteBuild.Stdout = os.Stdout
-	svelteBuild.Stderr = os.Stderr
-	svelteBuild.Run()
+	/*
+		svelteBuild := exec.Command("node", "ejected/build.js", clientBuildStr, staticBuildStr, allNodesStr)
+		svelteBuild.Stdout = os.Stdout
+		svelteBuild.Stderr = os.Stderr
+		svelteBuild.Run()
+	*/
+
+	ctx, _ := v8go.NewContext(nil)
+	content, err := ioutil.ReadFile("ejected/bundle.js")
+	if err != nil {
+		fmt.Printf("Could not read ejected/bundle.js file: %v", err)
+	}
+	val, err := ctx.RunScript(string(content), "ejected/bundle.js")
+	if err != nil {
+		fmt.Printf("Could not execute ejected/bundle.js file: %v", err)
+	}
+	fmt.Println(val)
 }
