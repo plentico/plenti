485b5b537cdbfb1d037360f50747afa955fd767f
jimafisk <jimafisk@gmail.com>
2020-08-25 20:43:06 -0400

    Add writable stores example to defaults.

--- defaults/layout/content/blog.svelte
+++ defaults/layout/content/blog.svelte
@@ -1,6 +1,15 @@
 <script>
-	export let title, body, author, date;
+	export let title, body, author, date, store;
   import Uses from "../components/template.svelte";
+
+  // Svelte store example:
+  import { count } from '../scripts/stores.svelte';
+  import Incrementer from '../components/incrementer.svelte';
+  import Decrementer from '../components/decrementer.svelte';
+  let count_value;
+  const unsubscribe = count.subscribe(value => {
+    count_value = value;
+  });
 </script>
 
 <h1>{title}</h1>
@@ -12,6 +21,12 @@
     <p>{@html paragraph}</p>
   {/each}
 </div>
+
+{#if store}
+  <h1>The count is {count_value}</h1>
+  <Incrementer/>
+  <Decrementer/>  
+{/if}
 
 <Uses type="blog" />
 
