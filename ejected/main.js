8a19a8e06e3de989c14ea0563882a147516fc81f
jimafisk <jimafisk@gmail.com>
2020-09-14 15:20:57 -0400

    Simplify html replacement (#65).

--- ejected/main.js
+++ ejected/main.js
@@ -1,30 +1,10 @@
 import Router from './router.svelte';
 
-/*
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/plenti-service-worker.js')
-  .then((reg) => {
-    console.log('Service Worker registration succeeded.');
-  }).catch((error) => {
-    console.log('Service Worker registration failed with ' + error);
-  });
-} else {
-  console.log('Service Workers not supported by browser')
-}
-*/
-
-const replaceContainer = function ( Component, options ) {
+const replaceContainer = Component => {
   const frag = document.createDocumentFragment();
-  const component = new Component( Object.assign( {}, options, { target: frag } ));
-  if (options.target) {
-    options.target.replaceWith( frag );
-  }
-  return component;
+  const dom = document.querySelector('#hydrate-plenti');
+  const component = new Component( Object.assign( {}, { target: dom }, { target: frag } ));
+  dom.replaceWith(frag);
 }
 
-const app = replaceContainer( Router, {
-  target: document.querySelector( '#hydrate-plenti' ),
-  props: {}
-});
-
-export default app;
+replaceContainer(Router);
