6a45b7e59b4ce1c97bfa69ac7ded1d9f47ec77eb
jimafisk <jimafisk@gmail.com>
2020-10-06 11:00:25 -0400

    Fix missing node_modules errors (#70).

--- cmd/build/npm_defaults.go
+++ cmd/build/npm_defaults.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"plenti/generated"
-	"strings"
 	"time"
 )
 
@@ -18,17 +17,15 @@ func NpmDefaults() {
 	Log("\nChecking if 'node_modules' directory exists.")
 
 	if _, err := os.Stat("node_modules"); os.IsNotExist(err) {
-		for file, content := range generated.Defaults {
-			if strings.HasPrefix(file, "/node_modules/") {
-				// Make file relative to where CLI is executed
-				file = "." + file
-				// Create the directories needed for the current file
-				os.MkdirAll(filepath.Dir(file), os.ModePerm)
-				// Create the current default file
-				err := ioutil.WriteFile(file, content, os.ModePerm)
-				if err != nil {
-					fmt.Printf("Unable to write npm dependency file: %v", err)
-				}
+		for file, content := range generated.Defaults_node_modules {
+			// Make file relative to where CLI is executed
+			file = "./node_modules/" + file
+			// Create the directories needed for the current file
+			os.MkdirAll(filepath.Dir(file), os.ModePerm)
+			// Create the current default file
+			err := ioutil.WriteFile(file, content, os.ModePerm)
+			if err != nil {
+				fmt.Printf("Unable to write npm dependency file: %v", err)
 			}
 		}
 	}
