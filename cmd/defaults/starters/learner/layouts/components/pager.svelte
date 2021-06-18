5375d20235292f162665fa66e3d7fd388fc3eb0d
jimafisk <jimafisk@gmail.com>
2021-06-17 23:19:44 -0400

    Make starter pager relative for baseurls (#68).

--- cmd/defaults/starters/learner/layouts/components/pager.svelte
+++ cmd/defaults/starters/learner/layouts/components/pager.svelte
@@ -4,26 +4,26 @@
 
 <ul>
   {#if currentPage > 1}
-    <li><a href="/">first</a></li>
-    <li><a href="/{currentPage - 1}">prev</a></li>
+    <li><a href=".">first</a></li>
+    <li><a href="{currentPage - 1}">prev</a></li>
   {:else}
     <li><span>first</span></li>
     <li><span>prev</span></li>
   {/if}
   {#each [3,2,1] as i}
     {#if currentPage - i > 0}
-      <li><a href="/{currentPage - i}">{currentPage - i}</a></li>
+      <li><a href="{currentPage - i}">{currentPage - i}</a></li>
     {/if}
   {/each}
   <li><span>{currentPage}</span></li>
   {#each Array(3) as _, i}
     {#if currentPage + (i+1) <= totalPages}
-      <li><a href="/{currentPage + (i+1)}">{currentPage + (i+1)}</a></li>
+      <li><a href="{currentPage + (i+1)}">{currentPage + (i+1)}</a></li>
     {/if}
   {/each}
   {#if currentPage < totalPages}
-    <li><a href="/{currentPage + 1}">next</a></li>
-    <li><a href="/{totalPages}">last</a></li>
+    <li><a href="{currentPage + 1}">next</a></li>
+    <li><a href="{totalPages}">last</a></li>
   {:else}
     <li><span>next</span></li>
     <li><span>last</span></li>
--- cmd/defaults/starters/learner/layouts/content/404.svelte
+++ cmd/defaults/starters/learner/layouts/content/404.svelte
@@ -1,2 +1,2 @@
 <h1>Oops... 404 not found</h1>
-<a href="/">Go home?</a>
+<a href=".">Go home?</a>
