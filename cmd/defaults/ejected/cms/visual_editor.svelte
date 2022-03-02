02ef4070a73c368cf747b39fd344897a5341f4fa
jimafisk <jimafisk@gmail.com>
2022-03-01 20:44:50 -0500

    Tie labels to input IDs.

--- cmd/defaults/ejected/cms/visual_editor.svelte
+++ cmd/defaults/ejected/cms/visual_editor.svelte
@@ -1,30 +1,34 @@
 <script>
-  export let content;
+    export let content;
 </script>
 
 <form>
-  {#each Object.entries(content.fields) as [label, field]}
-    <div class="field">
-      <label>{label}</label>
-      {#if field === null}
-          <div>{field} is null</div>
-      {:else if field === undefined}
-          <div>{field} is undefined</div>
-      {:else if field.constructor === "".constructor}
-        <input type="text" bind:value={content.fields[label]} />
-      {:else if field.constructor === true.constructor}
-        <input type="checkbox" bind:checked={content.fields[label]} /><span>{field}</span>
-      {:else if field.constructor === [].constructor}
-        {#each field as value, i}
-          <input bind:value={content.fields[label][i]} />
-        {/each}
-      {:else if field.constructor === ({}).constructor}
-        {#each Object.keys(field) as key}
-          <input bind:value={content.fields[label][key]} />
-        {/each}
-      {/if}
-    </div>
-  {/each}
+    {#each Object.entries(content.fields) as [label, field]}
+        <div class="field">
+            <label for="{label}">{label}</label>
+            {#if field === null}
+                <div>{field} is null</div>
+            {:else if field === undefined}
+                <div>{field} is undefined</div>
+            {:else if field.constructor === "".constructor}
+                {#if field.length < 1150}
+                    <input id="{label}" type="text" bind:value={content.fields[label]} />    
+                {:else}
+                    <textarea id="{label}" rows="5" bind:value={content.fields[label]}></textarea>    
+                {/if}
+            {:else if field.constructor === true.constructor}
+                <input id="{label}" type="checkbox" bind:checked={content.fields[label]} /><span>{field}</span>
+            {:else if field.constructor === [].constructor}
+                {#each field as value, i}
+                    <input id="{label}" bind:value={content.fields[label][i]} />
+                {/each}
+            {:else if field.constructor === ({}).constructor}
+                {#each Object.keys(field) as key}
+                    <input id="{label}" bind:value={content.fields[label][key]} />
+                {/each}
+            {/if}
+        </div>
+    {/each}
 </form>
 
 <style>
