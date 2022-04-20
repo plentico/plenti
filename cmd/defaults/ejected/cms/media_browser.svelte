cad3906bb016483ac4b65c1ac80d7bf5cc69f63a
jimafisk <jimafisk@gmail.com>
2022-04-20 18:42:24 -0400

    Subfolders as filters.

--- cmd/defaults/ejected/cms/media_browser.svelte
+++ cmd/defaults/ejected/cms/media_browser.svelte
@@ -5,6 +5,7 @@
     let assetsDir = env.baseurl ? 'assets/' : '/assets/';
     let links = new Promise(() => {});
     let allFiles = [];
+    let filters = [];
     const readDir = async (dir) => {
         links = await getAssets(dir);
         links.forEach(link => {
@@ -12,17 +13,33 @@
             if (linkPath.includes('.')) {
                 allFiles = [...allFiles, linkPath];
             } else {
+                let filter = link.innerHTML;
+                filter = filter.endsWith('/') ? filter.slice(0, -1) : filter;
+                filters = [...filters, filter];
                 readDir(linkPath);
             }
         });
     }
     readDir(assetsDir);
+
+    const limit = filter => {
+        allFiles = allFiles.filter(linkPath => {
+            let parts = linkPath.split("/");
+            return parts.includes(filter);
+        });
+    }
 </script>
 
-<div class="media-browser">
+<div class="media-wrapper">
 {#await links}
     Loading...    
 {:then _}
+    <div class="filters">
+    {#each filters as filter}
+        <div on:click={limit(filter)} class="filter">{filter}</div>
+    {/each}
+    </div>
+    <div class="media-browser">
     {#each allFiles as link}
         <div class="media">
             {#if link.endsWith('.pdf')}
@@ -31,13 +48,16 @@
                 <img src={link} />
             {/if}
         </div>
-    {/each}  
+    {/each}
+    </div>
 {/await}
 </div>
 
 <style>
-    .media-browser {
+    .media-wrapper {
         padding: 20px;
+    }
+    .media-browser {
         display: flex;
         flex-wrap: wrap;
         gap: 10px;
@@ -53,5 +73,15 @@
     }
     img, embed {
         max-width: 200px;
+    }
+    .filters {
+        padding: 10px 0;
+    }
+    .filter {
+        border-radius: 5px;
+        display: inline-block;
+        padding: 2px 10px;
+        border: 1px solid;
+        cursor: pointer;
     }
 </style>
