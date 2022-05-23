2d09319fba8b31d1c33d84d9426115ab8946f571
jimafisk <jimafisk@gmail.com>
2022-05-23 19:54:27 -0400

    Make modal appear above sidenav.

--- cmd/defaults/ejected/cms/admin_menu.svelte
+++ cmd/defaults/ejected/cms/admin_menu.svelte
@@ -93,37 +93,6 @@
   </a>
 </nav>
 
-<div class={showEditor ? "sidenav-wrapper" : ""}>
-{#if showEditor}
-  <div transition:horizontalSlide|local class={showEditor ? "sidenav" : ""}>
-    <div class="selectors">
-      <div class="selector {activeEditor === 'visual' ? 'active' : ''}" on:click={() => setActiveEditor('visual')}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-table" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <rect x="4" y="4" width="16" height="16" rx="2" />
-          <line x1="4" y1="10" x2="20" y2="10" />
-          <line x1="10" y1="4" x2="10" y2="20" />
-        </svg>
-        <span>Visual</span>
-      </div>
-      <div class="selector {activeEditor === 'code' ? 'active' : ''}" on:click={() => setActiveEditor('code')}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-braces" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
-          <path d="M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2" />
-        </svg>
-        <span>Code</span>
-      </div>
-    </div>
-    {#if activeEditor === 'code'}
-      <JSONEditor bind:content={content} />
-    {:else}
-      <VisualEditor bind:content={content} />
-    {/if}
-  </div>
-{/if}
-</div>
-
 {#if showMedia}
   <div class="modal-wrapper" on:click={toggleMedia}>
     <div class="modal-close">
@@ -163,6 +132,37 @@
     </div>
   </div>
 {/if}
+
+<div class={showEditor ? "sidenav-wrapper" : ""}>
+{#if showEditor}
+  <div transition:horizontalSlide|local class={showEditor ? "sidenav" : ""}>
+    <div class="selectors">
+      <div class="selector {activeEditor === 'visual' ? 'active' : ''}" on:click={() => setActiveEditor('visual')}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-table" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <rect x="4" y="4" width="16" height="16" rx="2" />
+          <line x1="4" y1="10" x2="20" y2="10" />
+          <line x1="10" y1="4" x2="10" y2="20" />
+        </svg>
+        <span>Visual</span>
+      </div>
+      <div class="selector {activeEditor === 'code' ? 'active' : ''}" on:click={() => setActiveEditor('code')}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-braces" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
+          <path d="M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2" />
+        </svg>
+        <span>Code</span>
+      </div>
+    </div>
+    {#if activeEditor === 'code'}
+      <JSONEditor bind:content={content} />
+    {:else}
+      <VisualEditor bind:content={content} />
+    {/if}
+  </div>
+{/if}
+</div>
 
 <style>
     .spacer {
