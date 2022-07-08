2e7a086a83ecf025a6fc586b9a963ad28483aabc
jimafisk <jimafisk@gmail.com>
2022-07-08 16:30:02 -0400

    Clear asset swap on modal close.

--- cmd/defaults/ejected/cms/admin_menu.svelte
+++ cmd/defaults/ejected/cms/admin_menu.svelte
@@ -14,6 +14,7 @@
     let showMedia = false;
     const toggleMedia = () => {
         showMedia = !showMedia;
+        changingAsset = "";
     }
     let activeMedia = "upload";
     const setActiveMedia = selected => {
