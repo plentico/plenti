031107c867de6db054faa796926e03fdbdccbef5
jimafisk <jimafisk@gmail.com>
2022-07-19 17:29:10 -0400

    Make filters buttons.

--- cmd/defaults/ejected/cms/media_filters.svelte
+++ cmd/defaults/ejected/cms/media_filters.svelte
@@ -89,19 +89,19 @@
     {#each filters as filterGroup}
         <div class="filter-group">
             {#each filterGroup as filter}
-                <div on:click={() => toggleFilter(getFilterSubGroup(filter, filterGroup))} class="filter{filterIsEnabled(enabledFilters, getFilterSubGroup(filter, filterGroup)) ? ' active' : ''}">{filter}</div>
+                <button on:click={() => toggleFilter(getFilterSubGroup(filter, filterGroup))} class="filter{filterIsEnabled(enabledFilters, getFilterSubGroup(filter, filterGroup)) ? ' active' : ''}">{filter}</button>
             {/each}
         </div>
     {/each}
     </div>
     {#if enabledFilters.length > 0}
-        <div on:click={clearFilters} class="close">
+        <button on:click={clearFilters} class="close">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="5 5 14 14" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <line x1="18" y1="6" x2="6" y2="18" />
                 <line x1="6" y1="6" x2="18" y2="18" />
             </svg>
-        </div>
+        </button>
     {/if}
 </div>
 
@@ -119,7 +119,6 @@
     }
     .filter-group {
         border-radius: 6px;
-        cursor: pointer;
         font-weight: bold;
         background-color: transparent;
         border: 2px solid #1c7fc7;
@@ -129,15 +128,21 @@
     .filter {
         display: inline-block;
         padding: 4px 10px;
+        color: #1c7fc7;
+        font-weight: bold;
     }
     .filter.active {
         background-color: #1c7fc7;
         color: white;
     }
     .close {
-        cursor: pointer;
-        padding: 5px 0;
+        padding: 3px 0;
         margin-left: auto;
         display: flex;
+    }
+    button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
     }
 </style>
