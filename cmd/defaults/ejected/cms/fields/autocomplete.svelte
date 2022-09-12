17c8c3a9f62897d92cdc91c02efd3538b3769e60
jimafisk <jimafisk@gmail.com>
2022-09-11 21:35:29 -0400

    Autocomplete loading spinner.

--- cmd/defaults/ejected/cms/fields/autocomplete.svelte
+++ cmd/defaults/ejected/cms/fields/autocomplete.svelte
@@ -2,9 +2,10 @@
     import allContent from '../../content.svelte';
     export let schema, parentKeys, field;
 
-    let input, results;
+    let input, results, loading;
 
     const search = () => {
+        loading = true;
         results = [];
         schema[parentKeys].options.forEach(option => {
             let filteredContent = allContent.filter(c => c.type = option.type);
@@ -19,16 +20,36 @@
         });
         if (input === "") {
             results = [];
-        } 
+        }
+        setTimeout(() => {
+            loading = false;
+        }, "500")
+    }
+    const makeSelection = () => {
+        results = [];
+        input = field;
     }
 </script>
 
 <div class="autocomplete">
     <input bind:value={input} on:keyup={search} />
+    <div class="load-icon">
+        {#if loading}
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-loader-2" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="gray" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 3a9 9 0 1 0 9 9"></path>
+            </svg>
+        {:else}    
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="gray" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <circle cx="12" cy="12" r="9"></circle>
+            </svg>
+        {/if}
+    </div>
     {#if results && results.length > 0}
         <select bind:value={field} size={results.length}>
             {#each results as result}
-                <option>{result}</option>
+                <option on:click={makeSelection}>{result}</option>
             {/each}
         </select>
     {/if}
@@ -54,5 +75,27 @@
     }
     option {
         padding: 7px;
+    }
+    .load-icon {
+        position: absolute;
+        right: 6px;
+        top: 8px;
+    }
+    .icon-tabler-loader-2 {
+        -webkit-animation: spin .5s linear infinite;
+        -moz-animation: spin .5s linear infinite;
+        animation: spin .5s linear infinite;
+    }
+    @-moz-keyframes spin { 
+        100% { -moz-transform: rotate(360deg); } 
+    }
+    @-webkit-keyframes spin { 
+        100% { -webkit-transform: rotate(360deg); } 
+    }
+    @keyframes spin { 
+        100% { 
+            -webkit-transform: rotate(360deg); 
+            transform:rotate(360deg); 
+        } 
     }
 </style>
