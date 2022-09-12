b2043698f12abe32915d29a740dca420cf57a855
jimafisk <jimafisk@gmail.com>
2022-09-11 20:46:59 -0400

    Clear autocomplete on empty input.

--- cmd/defaults/ejected/cms/fields/autocomplete.svelte
+++ cmd/defaults/ejected/cms/fields/autocomplete.svelte
@@ -10,21 +10,25 @@
             let filteredContent = allContent.filter(c => c.type = option.type);
             filteredContent.forEach(content => {
                 option.search.forEach(field => {
-                    if (content.fields[field].includes(input)) {
+                    if (content.fields.hasOwnProperty(field)
+                        && content.fields[field].includes(input)) {
                         results = [...results, content[option.result]];
                     }
                 });
             });
         });
+        if (input === "") {
+            results = [];
+        } 
     }
 </script>
 
-<input bind:value={input} on:keydown={search} />
+<input bind:value={input} on:keyup={search} />
 
 {#if results}
     <ul>
         {#each results as result}
-            <li>{result}</li>
+            <li on:click={() => field = result}>{result}</li>
         {/each}
     </ul>
 {/if}
