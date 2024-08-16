a9aeedd189c3aaec3c07a8b26c79eb12d0d8249c
jimafisk <jim.fisk@jantcu.com>
2024-08-16 14:01:37 -0400

    Allow N offset days in date widget (#336).

--- defaults/core/cms/fields/date.svelte
+++ defaults/core/cms/fields/date.svelte
@@ -3,13 +3,34 @@
 
     export let schema, parentKeys, field;
 
-    let today;
+    let today, todayPlusN, todayMinusN, N;
     if (schema && schema[parentKeys]?.options) {
         today = schema[parentKeys].options.includes("today");
+
+        const regex = /^today\s?(\+|\-)\s?([0-9]+)$/g;
+        const match = schema[parentKeys].options.find(option => regex.test(option));
+        if (match) {
+            const [, operation, daysOffset] = new RegExp(regex).exec(match);
+            todayPlusN = operation === "+";
+            todayMinusN = operation === "-";
+            N = parseInt(daysOffset);
+        }
     }
 
     if (today) {
         let date = new Date();
+        date = makeDate(date);
+        field = formatDate(date, field);
+    }
+    if (todayPlusN) {
+        let date = new Date();
+        date.setDate(date.getDate() + N);
+        date = makeDate(date);
+        field = formatDate(date, field);
+    }
+    if (todayMinusN) {
+        let date = new Date();
+        date.setDate(date.getDate() - N);
         date = makeDate(date);
         field = formatDate(date, field);
     }
