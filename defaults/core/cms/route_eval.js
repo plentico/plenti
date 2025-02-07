446dcf0b8a84e854c41341c2c503015427daf7e1
Jim Fisk <jim.fisk@jantcu.com>
2025-02-06 19:51:50 -0500

    Allow forward slashes in route_eval.

--- defaults/core/cms/route_eval.js
+++ defaults/core/cms/route_eval.js
@@ -33,7 +33,7 @@ function replaceRouteFields(route, fields) {
 function slugify(str) {
     str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
     str = str.toLowerCase(); // convert string to lowercase
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+    str = str.replace(/[^a-z0-9 -\/]/g, '') // remove any non-alphanumeric characters
              .replace(/\s+/g, '-') // replace spaces with hyphens
              .replace(/-+/g, '-'); // remove consecutive hyphens
     return str;
